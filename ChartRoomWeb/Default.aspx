204c02c6718968b78ceba9a0e351f56a28e86786
laerton2016 <laerton240311@gmail.com>
2018-02-23 07:27:51 -0300

    Inciado o projeto

---
+++ ChartRoomWeb/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>ChatRoom - Login </title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+    <script src="js/index.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <div class="wrap">
+            <div><h3 style= "text-align: center; color: white ">ChatRoom Login</h3></div>
+            <div class="avatar">
+                <img  src="/img/avatar.png"/>
+            </div>
+            <input type="text" placeholder="username" required runat="server"/>
+            <div class="bar">
+                <i></i>
+            </div>
+            <input type="password" placeholder="password" required runat="server"/>
+            <a href="" class="forgot_link">esqueceu ?</a>
+            <button>Sign in</button>
+        </div>
+
+        
+
+    </form>
+</body>
+</html>
+
---
+++ ChatRoomCore/model/ChatUser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatRoomCore.model
+{
+    /// <summary>
+    /// Classe de Usuário que partcipa de um chat 
+    /// </summary>
+    public class ChatUser:  IDisposable
+    {
+        /// <summary>
+        /// Id do usuário
+        /// </summary>
+        public UInt32 UserId { get; set; }
+        /// <summary>
+        /// Nome do Usuário
+        /// </summary>
+        public String UserName { get; set; }
+        /// <summary>
+        /// Status de ativo do usuário
+        /// </summary>
+        public bool IsActive { get; set; }
+        /// <summary>
+        /// Última vizualisação do úsuário
+        /// </summary>
+        public DateTime LastSeen { get; set; }
+        /// <summary>
+        /// Int da última mensagem recebida.
+        /// </summary>
+        public int LastMenssageRecived { get; set; }
+        /// <summary>
+        /// Controi um usuário 
+        /// </summary>
+        /// <param name="userId">Id do usuário </param>
+        /// <param name="userName">Nome do usuário</param>
+        public ChatUser(UInt32 userId, string userName)
+        {
+            this.UserId = userId;
+            this.IsActive = false;
+            this.LastSeen = DateTime.MinValue;
+            this.UserName = userName;
+            this.LastMenssageRecived = 0;
+        }
+        /// <summary>
+        /// Desfaz um usuário
+        /// </summary>
+        public void Dispose()
+        {
+            this.UserId = 0;
+            this.IsActive = false;
+            this.LastSeen = DateTime.MinValue;
+            this.UserName = "";
+            this.LastMenssageRecived = 0;
+        }
+    }
+}
